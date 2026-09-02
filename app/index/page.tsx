1f75b9e4f964254cef623d933b54836654308cfc
unknown <dxia4735@gmail.com>
2026-09-03 01:09:58 +0800

    avoid vercel onBuildComplete index segment error

--- app/index/page.tsx
+++
@@ -1,2 +0,0 @@
-import Home from '../page';
-export default function Index(){return <Home/>}
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [{ source: '/index', destination: '/' }];
+  },
+};
+module.exports = nextConfig;
